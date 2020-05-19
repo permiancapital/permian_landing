97633f5f61492323babeb0dc8fb961f8a4e970e5
Matthew Moravec <matthewmoravec@Matthews-MacBook-Pro.local>
2020-05-19 09:29:38 -0700

    add menu

--- src/app/Home/Menu.jsx
+++ src/app/Home/Menu.jsx
@@ -1,47 +1,31 @@
-import React, { useState } from "react";
-import {
-  Toolbar,
-  TextField,
-  List,
-  ListItem,
-  Divider,
-  Button,
-  LogoIcon,
-} from "react95";
+import React, { useState } from 'react'
+import { Toolbar, TextField, List, ListItem, Divider, Button, LogoIcon } from 'react95'
 
 export default function Menu() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false)
 
   function handleClick() {
-    setOpen(!open);
+    setOpen(!open)
   }
 
   function handleClose() {
-    setOpen(false);
+    setOpen(false)
   }
 
   return (
-    <div style={{ position: "relative", display: "inline-block" }}>
+    <div style={{ position: 'relative', display: 'inline-block' }}>
       {open && (
-        <List
-          horizontalAlign="left"
-          verticalAlign="bottom"
-          open={open}
-          onClick={handleClose}
-        >
+        <List horizontalAlign="left" verticalAlign="bottom" open={open} onClick={handleClose}>
           <ListItem>🏛 About</ListItem>
           <ListItem>📁 Latest Posts</ListItem>
+          <ListItem>🐳 EzeDex</ListItem>
           <Divider />
           <ListItem disabled>🔙 Logout</ListItem>
         </List>
       )}
-      <Button
-        onClick={handleClick}
-        active={open}
-        style={{ fontWeight: "bold" }}
-      >
+      <Button onClick={handleClick} active={open} style={{ fontWeight: 'bold' }}>
         Start
       </Button>
     </div>
-  );
+  )
 }
---
+++ src/app/Home/Tickers.jsx
@@ -0,0 +1,2 @@
+import React, { useState } from 'react'
+import { AppBar, Toolbar, TextField, List, ListItem, Divider, Button, LogoIcon } from 'react95'
